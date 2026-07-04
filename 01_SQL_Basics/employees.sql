ed0b040464b77cd0315348c8cd13cb3ca1515687
vishwa-2912 <vishwajeethgumudala@gmail.com>
2026-07-05 03:19:42 +0530

    Create employees table in SQL

---
+++ 01_SQL_Basics/employees.sql
@@ -0,0 +1,11 @@
+-- Create Employees Table
+
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    age INT,
+    department VARCHAR(50),
+    salary DECIMAL(10,2),
+    city VARCHAR(50)
+);
