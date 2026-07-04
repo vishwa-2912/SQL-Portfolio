aa4ef87c85f8a38771058ec7ecfd0816142b9592
vishwa-2912 <vishwajeethgumudala@gmail.com>
2026-07-05 03:36:22 +0530

    Add SQL script to insert department records

---
+++ 03_Joins/departments_data.sql
@@ -0,0 +1,10 @@
+-- Insert Department Records
+
+INSERT INTO departments
+(department_id, department_name, manager_name)
+VALUES
+(1,'IT','Robert'),
+(2,'HR','Jennifer'),
+(3,'Finance','Michael'),
+(4,'Sales','Andrew'),
+(5,'Marketing','Sophia');
