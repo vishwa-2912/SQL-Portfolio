a0ca86a8fd2222b94c6b357171443db2049ecd20
vishwa-2912 <vishwajeethgumudala@gmail.com>
2026-07-05 03:40:37 +0530

    Add self join query for employees in the same city
    This query compares employees working in the same city.

---
+++ 03_Joins/05_SELF_JOIN.sql
@@ -0,0 +1,14 @@
+-- SELF JOIN
+-- Compare employees working in the same city.
+
+SELECT
+    A.first_name AS Employee1,
+    B.first_name AS Employee2,
+    A.city
+FROM employees A
+JOIN employees B
+ON A.city = B.city
+AND A.emp_id <> B.emp_id;
+
+-- Output:
+-- Employees living in the same city.
