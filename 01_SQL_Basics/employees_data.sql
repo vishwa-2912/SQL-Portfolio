67956feaa011febe1ad64287c919bc93f2a21d2f
vishwa-2912 <vishwajeethgumudala@gmail.com>
2026-07-05 03:38:15 +0530

    Change department names to department IDs in employee records
    Updated department field to use department IDs instead of names.

--- 01_SQL_Basics/employees_data.sql
+++ 01_SQL_Basics/employees_data.sql
@@ -1,15 +1,13 @@
--- Insert Employee Records
-
 INSERT INTO employees
-(emp_id, first_name, last_name, age, department, salary, city)
+(emp_id,first_name,last_name,age,department_id,salary,city)
 VALUES
-(101,'John','Smith',25,'IT',50000,'London'),
-(102,'Sarah','Johnson',30,'HR',45000,'Manchester'),
-(103,'David','Brown',28,'IT',60000,'London'),
-(104,'Emma','Wilson',35,'Finance',75000,'Birmingham'),
-(105,'Michael','Taylor',26,'Sales',42000,'Leeds'),
-(106,'Sophia','Davis',32,'IT',68000,'London'),
-(107,'James','Miller',40,'Finance',85000,'Liverpool'),
-(108,'Olivia','Moore',29,'HR',47000,'Manchester'),
-(109,'Daniel','Thomas',27,'Sales',40000,'Leeds'),
-(110,'Emily','White',31,'IT',62000,'London');
+(101,'John','Smith',25,1,50000,'London'),
+(102,'Sarah','Johnson',30,2,45000,'Manchester'),
+(103,'David','Brown',28,1,60000,'London'),
+(104,'Emma','Wilson',35,3,75000,'Birmingham'),
+(105,'Michael','Taylor',26,4,42000,'Leeds'),
+(106,'Sophia','Davis',32,1,68000,'London'),
+(107,'James','Miller',40,3,85000,'Liverpool'),
+(108,'Olivia','Moore',29,2,47000,'Manchester'),
+(109,'Daniel','Thomas',27,4,40000,'Leeds'),
+(110,'Emily','White',31,1,62000,'London');
