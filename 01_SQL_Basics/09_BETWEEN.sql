df49b7fcdc2bad7c0a0a31dfc028f2cf0343d49e
vishwa-2912 <vishwajeethgumudala@gmail.com>
2026-07-05 03:24:48 +0530

    Add SQL query to select employees by salary range
    This SQL query retrieves employees with salaries between 45000 and 70000.

---
+++ 01_SQL_Basics/09_BETWEEN.sql
@@ -0,0 +1,8 @@
+-- Employees with salaries between 45000 and 70000
+
+SELECT *
+FROM employees
+WHERE salary BETWEEN 45000 AND 70000;
+
+-- Output:
+-- Displays employees whose salary falls within the specified range.
