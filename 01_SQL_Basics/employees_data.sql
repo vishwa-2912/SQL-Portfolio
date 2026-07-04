14e4ebf110ec52115e61cb1bbc9517bbcf2abdd3
vishwa-2912 <vishwajeethgumudala@gmail.com>
2026-07-05 03:20:13 +0530

    Add employee records to employees_data.sql
    Inserted multiple employee records into the employees table.

---
+++ 01_SQL_Basics/employees_data.sql
@@ -0,0 +1,15 @@
+-- Insert Employee Records
+
+INSERT INTO employees
+(emp_id, first_name, last_name, age, department, salary, city)
+VALUES
+(101,'John','Smith',25,'IT',50000,'London'),
+(102,'Sarah','Johnson',30,'HR',45000,'Manchester'),
+(103,'David','Brown',28,'IT',60000,'London'),
+(104,'Emma','Wilson',35,'Finance',75000,'Birmingham'),
+(105,'Michael','Taylor',26,'Sales',42000,'Leeds'),
+(106,'Sophia','Davis',32,'IT',68000,'London'),
+(107,'James','Miller',40,'Finance',85000,'Liverpool'),
+(108,'Olivia','Moore',29,'HR',47000,'Manchester'),
+(109,'Daniel','Thomas',27,'Sales',40000,'Leeds'),
+(110,'Emily','White',31,'IT',62000,'London');
