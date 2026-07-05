859fdb4474b99e3af6d4b719ff128669b15575b5
vishwa-2912 <vishwajeethgumudala@gmail.com>
2026-07-06 01:38:46 +0530

    Create 04_Check_Data.sql for data verification queries
    Added queries to check data for departments, jobs, employees, projects, and employee projects.

---
+++ 10_SQL_Projects/Employee_Management_System/04_Check_Data.sql
@@ -0,0 +1,19 @@
+-- Total Departments
+
+SELECT * FROM departments;
+
+-- Total Jobs
+
+SELECT * FROM jobs;
+
+-- Total Employees
+
+SELECT * FROM employees;
+
+-- Total Projects
+
+SELECT * FROM projects;
+
+-- Employee Projects
+
+SELECT * FROM employee_projects;
