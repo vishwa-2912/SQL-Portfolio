00bd91843eb1ffaae1f470c1f8877d1e3101f78b
vishwa-2912 <vishwajeethgumudala@gmail.com>
2026-07-06 01:35:45 +0530

    Create tables for departments, jobs, employees, projects

--- 10_SQL_Projects/Employee_Management_System/02_Create_Tables.sql
+++ 10_SQL_Projects/Employee_Management_System/02_Create_Tables.sql
@@ -4,3 +4,78 @@ CREATE TABLE departments
     department_name VARCHAR(50),
     manager_name VARCHAR(100)
 );
+
+
+--jobs
+CREATE TABLE jobs
+(
+    job_id INT PRIMARY KEY,
+    job_title VARCHAR(50),
+    minimum_salary DECIMAL(10,2),
+    maximum_salary DECIMAL(10,2)
+);
+
+--employess
+CREATE TABLE employees
+(
+    employee_id INT PRIMARY KEY,
+
+    first_name VARCHAR(50),
+
+    last_name VARCHAR(50),
+
+    gender VARCHAR(10),
+
+    age INT,
+
+    city VARCHAR(50),
+
+    hire_date DATE,
+
+    salary DECIMAL(10,2),
+
+    department_id INT,
+
+    job_id INT,
+
+    FOREIGN KEY(department_id)
+    REFERENCES departments(department_id),
+
+
+-projects
+    CREATE TABLE projects
+(
+    project_id INT PRIMARY KEY,
+
+    project_name VARCHAR(100),
+
+    budget DECIMAL(12,2),
+
+    department_id INT,
+
+    FOREIGN KEY(department_id)
+    REFERENCES departments(department_id)
+);
+
+
+-employee projects
+    CREATE TABLE employee_projects
+(
+    employee_id INT,
+
+    project_id INT,
+
+    PRIMARY KEY(employee_id,project_id),
+
+    FOREIGN KEY(employee_id)
+    REFERENCES employees(employee_id),
+
+    FOREIGN KEY(project_id)
+    REFERENCES projects(project_id)
+);
+
+
+
+    FOREIGN KEY(job_id)
+    REFERENCES jobs(job_id)
+);
